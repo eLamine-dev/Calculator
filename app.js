710d952bcd7f07c75e49e28d431a866313cc7586
e-lamine <lamine@e-lamine.dev>
2022-11-14 11:30:17 +0100

    power calculation bug fixed

--- app.js
+++ app.js
@@ -2,39 +2,43 @@ let Calculator = {
     firstOperand: '',
     secondOperand: '',
     operator: '',
-    result: '',
-    operate: function(a, b, operator){
+    result: function(a, b, operator){
         a = Number(this.firstOperand);
         b = Number(this.secondOperand);
         operator = this.operator;
         switch (operator) {
             case '+':
-                this.result = add(a, b)
+                return add(a, b)
                 break;
             case '-':
-                this.result = subtract(a, b)
+                return subtract(a, b)
                 break;
             case '/' :
-                this.result = divide(a, b)
+                return divide(a, b)
                 break;
             case '*':
-                this.result = multiply(a, b)
+                return multiply(a, b)
                 break; 
             case '^':
-                this.result = power(a, b)
+                return power(a, b)
                 break;
             case '%':
-                this.result = percentage(a)
+                return percentage(a)
                 break;  
             case 'sqrt':
-                this.result = squareRoot(a)
+                return squareRoot(a)
                 break; 
             case 'mod':
-                this.result = modulo(a,b)
+                return modulo(a,b)
                 break; 
+            case 'neg':
+                return NegPosToggle(a)
+                break; 
+            default :
+                return ''
         }
-    }
-        
+    },
+      
 }
 
 const operationsButtons = document.querySelectorAll('.op');
@@ -53,18 +57,15 @@ operationsButtons.forEach(btn => {
 
 
 function handleEqualBtn(btn) {
-    if (Calculator.firstOperand === '' ) {
-
+    if (Calculator.firstOperand === '') {
+        
     } else if (Calculator.firstOperand !== '' && screenMain.innerText === ''){
-        Calculator.result = Calculator.firstOperand;
-        screenMain.innerText = Calculator.result;
-        screenSecond.innerText = ``;
+        screenMain.innerText = Calculator.firstOperand;
+        screenSecond.innerText = '';
         Calculator.firstOperand = '';
-    } else {
+    } else if (Calculator.firstOperand !== '' && screenMain.innerText !== '') {
             Calculator.secondOperand = screenMain.innerText;
-            Calculator.operate();
-            screenMain.innerText = `${Calculator.result}`;
-            Calculator.operator = '';
+            screenMain.innerText = Calculator.result();
             screenSecond.innerText += ` ${Calculator.secondOperand}`
             Calculator.firstOperand = '';
 
@@ -81,25 +82,24 @@ function handleTwoOperandsOperations(btn) {
     } else if (Calculator.firstOperand !== '') {
         if (screenMain.innerText === ''){
             Calculator.operator = btn.value;
-            screenSecond.innerText = `${Calculator.result || Calculator.firstOperand} ${btn.innerText}`;
+            screenSecond.innerText = `${Calculator.firstOperand} ${btn.innerText}`;
         } else {
             Calculator.secondOperand = screenMain.innerText;
-            Calculator.operate();
             screenMain.innerText = '';
             Calculator.operator = btn.value;
-            screenSecond.innerText = ` ${Calculator.result} ${btn.innerText}`
-            Calculator.firstOperand = Calculator.result;   
+            screenSecond.innerText = ` ${Calculator.result()} ${btn.innerText}`
+            Calculator.firstOperand = Calculator.result();   
         }
     }
 }
 
 function handleOneOperandOperations(btn) {
-
-        Calculator.firstOperand = screenMain.innerText || Calculator.result; 
+        Calculator.firstOperand = screenMain.innerText || Calculator.firstOperand; 
         Calculator.operator = btn.value;
-        Calculator.operate();
-        screenMain.innerText = Calculator.result;
+        screenMain.innerText = Calculator.result();
         screenSecond.innerText = '';
+        Calculator.firstOperand = ''; 
+        
 }
 
 
@@ -112,45 +112,9 @@ numbersButtons.forEach(btn => {
 
     btn.addEventListener('click', ()=>{
         if (screenMain.innerText == "0" || screenMain.innerText == Calculator.result ) {screenMain.innerText = btn.id}
-        else {screenMain.innerText += btn.id;}
+        else if (screenMain.innerText.length < 15) {screenMain.innerText += btn.id;}
     })
 });
-
-
-
-// function operate(a, b, operator){
-//     a = Number(a);
-//     b = Number(b);
-//     let result;
-//     switch (operator) {
-//         case '+':
-//             result = add(a, b)
-//             break;
-//         case '-':
-//             result = subtract(a, b)
-//             break;
-//         case '/' :
-//             result = divide(a, b)
-//             break;
-//         case '*':
-//             result = multiply(a, b)
-//             break; 
-//         case '^':
-//             result = power(a, b)
-//             break;
-//         case '%':
-//             result = percentage(a)
-//             break;  
-//         case 'sqrt':
-//             result = squareRoot(a)
-//             break; 
-//         case 'mod':
-//             result = modulo(a,b)
-//             break;  
-//     }
-//     return result;
-// }
-
 
 
 const add = function(a, b) {
@@ -178,11 +142,15 @@ const percentage = function(a) {
 };
 
 const squareRoot = function(a) {
-	return Math. sqrt(a);
+	return Math.sqrt(a);
 };
 
 const modulo = function(a, b) {
     return a%b;
+}
+
+const NegPosToggle = function(a) {
+    return 0-a;
 }
 
 
